0565dda1a338f44a56513e10e1ae666885742b78
Igor Maslov <imaslov@bk.ru>
2022-05-17 18:21:59 +0300

    It's my first telegram BOT

---
+++ bot.py
@@ -0,0 +1,33 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters 
+import settings
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+
+#PROXY = {'proxy_url': settings.PROXY_URL,
+#    'urllib3_proxy_kwargs': {'username': settings.ROXY_USERNAME, 'password': settings.ROXY_PASSWORD}}
+
+def greet_user(update, context):
+    print('Вызван /start')
+    update.message.reply_text('Приветствую, высокочтимый друг!')
+
+def talk_to_me(update, context):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+
+def main():
+    mybot = Updater(settings.API_KEY, use_context=True)#, request_kwargs=PROXY)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    logging.info('Бот стартовал') # ДЗ: отобразить дату и время в логах, см. работу logging
+    mybot.start_polling()
+    mybot.idle()
+
+if __name__=='__main__':
+    main()
+
